a1b5fb9c4fe9c53f61c1e7ea597b32413088b5ac
Alison Zerbe <alisonkzerbe@gmail.com>
2025-10-18 15:19:50 -0700

    q1 refactor into nested dict

--- q1/create_2gram_hmm.py
+++ q1/create_2gram_hmm.py
@@ -1,37 +1,56 @@
 import sys
+STATE_DICT = {}
 
-def create_token_map(line):
+def get_transitions(bigram_state_dict):
+    current_w1 =''
+    state_nums = 0
+    for key, value in bigram_state_dict.items():
+        split_token = key.split(" ")
+        token_w1 = split_token[0]
+        if token_w1 == current_w1:
+            print(f"SAME {token_w1}")
+        else:
+            print(f"new w1 {token_w1}")
+            state_nums +=1
+            current_w1 = token_w1
+
+    return state_nums
+
+def create_state_dict(line):
     split_line = line.split(" ")
-    state_dict = {}
 
     for i, token in enumerate(split_line):
-        if i > 0:
-            bigram_first = split_line[i-1].split("/")
-            if i == len(split_line ) - 1:
-                bigram_no_EOS = split_line[i].split("</s>")
-                bigram_first = bigram_no_EOS[0].split('/')[0]
-                bigram_second = '</s>'
-                bigram_pos = f"{bigram_first} {bigram_second}"
+        if i < len(split_line)-2:
+            token_1 = token.split("/")
+            token_2 = split_line[i+1].split("/")
+        else:
+            token_no_EOS = token.split("</s>")
+            token_1 = token_no_EOS[0].split("/")
+            token_2 = ['', "</s>"]
+    
+        POS_1 = token_1[1]
+        POS_2 = token_2[1]
+
+        if POS_1 in STATE_DICT:
+            if POS_2 in STATE_DICT[POS_1]:
+                STATE_DICT[POS_1][POS_2] +=1
             else:
-                bigram_second = split_line[i].split("/")
-                bigram_pos = f"{bigram_first[1]} {bigram_second[1]}"
-            if bigram_pos in state_dict:
-                state_dict[bigram_pos] +=1
-            else: 
-                state_dict[bigram_pos] = 1
-    sorted_dict = sorted(state_dict.items(), key=lambda item:item[0])
-    return sorted_dict
+                STATE_DICT[POS_1].update({POS_2: 1})
+        else:
+            new_entry = {POS_1: {POS_2: 1}}
+            STATE_DICT.update(new_entry)
 
 def read_input():
     lines = sys.stdin.readlines()
     for line in lines:
         EOS_line = line.rstrip() + '</s>'
-        bigram_token_map = create_token_map(EOS_line)
-    return bigram_token_map
+        create_state_dict(EOS_line)
 
 def main():
-    bigram_token_map = read_input()
-    print(f"BIGGGG", bigram_token_map)
+    read_input()
+    # state_nums = get_transitions(bigram_token_dict)
+
+    print(f"BIGGGG {STATE_DICT}")
 
 
 main()
