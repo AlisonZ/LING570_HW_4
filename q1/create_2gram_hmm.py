73dfcd513834dd24bfd8795077a33b480ad745fd
Alison Zerbe <alisonkzerbe@gmail.com>
2025-10-18 17:21:20 -0700

    q1: adds emissions counts for each token

--- q1/create_2gram_hmm.py
+++ q1/create_2gram_hmm.py
@@ -1,5 +1,6 @@
 import sys
 STATE_DICT = {}
+POS_DICT = {}
 
 def get_total(transition_dict):
     total = 0
@@ -7,19 +8,30 @@ def get_total(transition_dict):
         count = v['count']
         total += count
     return total
-    
+
+def get_emissions():
+    for k, v in POS_DICT.items():
+        print(f"key: {k} value: {v}")
 
 def get_transitions():
     for key, value in STATE_DICT.items():
         transitions = STATE_DICT[key]
         total_sum = get_total(transitions)
-        # print(f"KEY:::{key}")
 
         for k, v in transitions.items():
             prob = v['count'] / total_sum
             STATE_DICT[key][k]['prob'] = prob
-            # print(f"XXX {}")
 
+def update_POS_dict(token):
+    word = token[0]
+    pos_tag = token[1]
+    if pos_tag in POS_DICT:
+        if  word in POS_DICT[pos_tag]:
+            POS_DICT[pos_tag][word] +=1 
+        else:
+            POS_DICT[pos_tag] = {word : 1}
+    else:
+        POS_DICT[pos_tag] = {word: 1}
 
 def create_state_dict(line):
     split_line = line.split(" ")
@@ -32,6 +44,7 @@ def create_state_dict(line):
             token_no_EOS = token.split("</s>")
             token_1 = token_no_EOS[0].split("/")
             token_2 = ['', "</s>"]
+        update_POS_dict(token_1)
     
         POS_1 = token_1[1]
         POS_2 = token_2[1]
@@ -54,8 +67,5 @@ def read_input():
 def main():
     read_input()
     get_transitions()
-
-    print(f"BIGGGG {STATE_DICT}")
-
-
+    get_emissions()
 main()
